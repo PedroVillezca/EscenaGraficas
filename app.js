1876d6fb6f25ded9449fccce4c689c36a4fc48a7
Gibran Gonzalez <hector.gibran@hotmail.com>
2021-01-26 16:38:48 -0600

    Adds THREE js library to project, creates new scene with sample cube

--- app.js
+++ app.js
@@ -4,87 +4,127 @@ Gibran Gonzalez A01282778
 Guillermo Saldaña A01039888
 */
 
-var vertexShaderText = 
-[
-'precision mediump float;',
-'',
-'attribute vec2 vertPosition;',
-'attribute vec3 vertColor;',
-'varying vec3 fragColor;',
-'',
-'void main()',
-'{',
-'  fragColor = vertColor;',
-'  gl_Position = vec4(vertPosition, 0.0, 1.0);',
-'}'
+import * as THREE from './node_modules/three/src/Three.js';
+
+const vertexShaderText = [
+  'precision mediump float;',
+  '',
+  'attribute vec2 vertPosition;',
+  'attribute vec3 vertColor;',
+  'varying vec3 fragColor;',
+  '',
+  'void main()',
+  '{',
+  '  fragColor = vertColor;',
+  '  gl_Position = vec4(vertPosition, 0.0, 1.0);',
+  '}',
 ].join('\n');
 
-var fragmentShaderText =
+const fragmentShaderText =
 [
-'precision mediump float;',
-'',
-'varying vec3 fragColor;',
-'void main()',
-'{',
-'  gl_FragColor = vec4(fragColor, 1.0);',
-'}'
+  'precision mediump float;',
+  '',
+  'varying vec3 fragColor;',
+  'void main()',
+  '{',
+  '  gl_FragColor = vec4(fragColor, 1.0);',
+  '}',
 ].join('\n');
 
+/**
+ * Main function
+ */
 function main() {
-    const canvas = document.querySelector('#glcanvas');
+  /* const canvas = document.querySelector('#glcanvas');
 
-    const gl = canvas.getContext('webgl');
+  const gl = canvas.getContext('webgl');
 
-    if (!gl) {
-		console.log('WebGL not supported, falling back on experimental-webgl');
-		gl = canvas.getContext('experimental-webgl');
-    }
-    
-    if (!gl) {
-    alert('Unable to initialize WebGL. Your browser or machine may not support it.');
+  if (!gl) {
+    console.log('WebGL not supported, falling back on experimental-webgl');
+    gl = canvas.getContext('experimental-webgl');
+  }
+
+  if (!gl) {
+    alert('Unable to initialize WebGL.' +
+        'Your browser or machine may not support it.');
     return;
-    }
+  }
 
-    // Set clear color to black, fully opaque
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    // Clear the color buffer with specified clear color
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  // Set clear color to black, fully opaque
+  // gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // Clear the color buffer with specified clear color
+  // gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    //
-	// Create shaders
-	// 
-	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-	var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  //
+  // Create shaders
+  //
+  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 
-    gl.shaderSource(vertexShader, vertexShaderText);
-    gl.shaderSource(fragmentShader, fragmentShaderText);
+  gl.shaderSource(vertexShader, vertexShaderText);
+  gl.shaderSource(fragmentShader, fragmentShaderText);
 
-    gl.compileShader(vertexShader);
-    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-        console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
-        return;
-    }
+  gl.compileShader(vertexShader);
+  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    console.error(
+        'ERROR compiling vertex shader!',
+        gl.getShaderInfoLog(vertexShader),
+    );
+    return;
+  }
 
-    gl.compileShader(fragmentShader);
-    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-        console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
-        return;
-    }
+  gl.compileShader(fragmentShader);
+  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    console.error(
+        'ERROR compiling fragment shader!',
+        gl.getShaderInfoLog(fragmentShader),
+    );
+    return;
+  }
 
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        console.error('ERROR linking program!', gl.getProgramInfoLog(program));
-        return;
-    }
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error('ERROR linking program!', gl.getProgramInfoLog(program));
+    return;
+  }
 
-    gl.validateProgram(program);
-    if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
-        console.error('ERROR validating program!', gl.getProgramInfoLog(program));
-        return;
-    }
+  gl.validateProgram(program);
+  if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+    console.error('ERROR validating program!', gl.getProgramInfoLog(program));
+    return;
+  }*/
+
+
+  /* ========= EJEMPLO THREE JS =========== */
+  const scene = new THREE.Scene();
+  const camera = new THREE
+      .PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setClearColor('#000000');
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+  const cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+
+  camera.position.z = 5;
+
+  const animate = function() {
+    requestAnimationFrame( animate );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+  };
+
+  animate();
 }
 
 main();
