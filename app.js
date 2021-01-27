bb9e8c3f52f9910e75af5017a7b0c72856aa4825
Gibran Gonzalez <hector.gibran@hotmail.com>
2021-01-26 19:24:14 -0600

    Adds models for walls, adds camera controls to scene

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable require-jsdoc */
 /*
 Pedro Villezca A01282613
 Gibran Gonzalez A01282778
@@ -5,126 +6,151 @@ Guillermo Saldaña A01039888
 */
 
 import * as THREE from './node_modules/three/src/Three.js';
+import {OrbitControls} from
+  './node_modules/three/examples/jsm/controls/OrbitControls.js';
 
-const vertexShaderText = [
-  'precision mediump float;',
-  '',
-  'attribute vec2 vertPosition;',
-  'attribute vec3 vertColor;',
-  'varying vec3 fragColor;',
-  '',
-  'void main()',
-  '{',
-  '  fragColor = vertColor;',
-  '  gl_Position = vec4(vertPosition, 0.0, 1.0);',
-  '}',
-].join('\n');
+const scene = new THREE.Scene();
 
-const fragmentShaderText =
-[
-  'precision mediump float;',
-  '',
-  'varying vec3 fragColor;',
-  'void main()',
-  '{',
-  '  gl_FragColor = vec4(fragColor, 1.0);',
-  '}',
-].join('\n');
+const camera = new THREE
+    .PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setClearColor('#000000');
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+window.addEventListener('resize', () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+
+  camera.updateProjectionMatrix();
+});
+
 
 /**
- * Main function
+ * Creates a conveyor belt on x,y,z coordinates and
+ * rotates it in y in terms of angle given
+ * @param {Number} x
+ * @param {Number} y
+ * @param {Number} z
+ * @param {Number} angle
  */
+function createZConveyorBelt(x, y, z) {
+  const mainStructureGeom = new THREE.BoxGeometry(5, 0.5, 15);
+  const mainStructureMaterial = new THREE
+      .MeshLambertMaterial({color: 0xbfc7cc});
+
+  const mainStructure = new THREE
+      .Mesh(mainStructureGeom, mainStructureMaterial);
+  mainStructure.position.set(0+x, 3+y, 0+z);
+  scene.add(mainStructure);
+
+  const legGeom = new THREE.CylinderGeometry(0.5, 0.5, 3);
+  const legOne = new THREE.Mesh(legGeom, mainStructureMaterial);
+  const legTwo = new THREE.Mesh(legGeom, mainStructureMaterial);
+  const legThree = new THREE.Mesh(legGeom, mainStructureMaterial);
+  const legFour = new THREE.Mesh(legGeom, mainStructureMaterial);
+  legOne.position.set(-2+x, 1.5+y, -7+z);
+  legTwo.position.set(2+x, 1.5+y, -7+z);
+  legThree.position.set(2+x, 1.5+y, 7+z);
+  legFour.position.set(-2+x, 1.5+y, 7+z);
+  scene.add(legOne);
+  scene.add(legTwo);
+  scene.add(legThree);
+  scene.add(legFour);
+}
+
+function createXConveyorBelt(x, y, z) {
+  const mainStructureGeom = new THREE.BoxGeometry(10, 0.5, 5);
+  const mainStructureMaterial = new THREE
+      .MeshLambertMaterial({color: 0xbfc7cc});
+
+  const mainStructure = new THREE
+      .Mesh(mainStructureGeom, mainStructureMaterial);
+  mainStructure.position.set(0+x, 3+y, 0+z);
+  scene.add(mainStructure);
+
+  const legGeom = new THREE.CylinderGeometry(0.5, 0.5, 3);
+  const legOne = new THREE.Mesh(legGeom, mainStructureMaterial);
+  const legTwo = new THREE.Mesh(legGeom, mainStructureMaterial);
+  const legThree = new THREE.Mesh(legGeom, mainStructureMaterial);
+  const legFour = new THREE.Mesh(legGeom, mainStructureMaterial);
+  legOne.position.set(-4+x, 1.5+y, -2+z);
+  legTwo.position.set(4+x, 1.5+y, -2+z);
+  legThree.position.set(4+x, 1.5+y, 2+z);
+  legFour.position.set(-4+x, 1.5+y, 2+z);
+  scene.add(legOne);
+  scene.add(legTwo);
+  scene.add(legThree);
+  scene.add(legFour);
+}
+
+function objectSetup() {
+  const spawnObjectGeometry = new THREE.BoxGeometry(5, 8, 5);
+  const spawnObjectMaterial = new THREE.MeshLambertMaterial(
+      {color: 0xbfc7cc},
+  );
+
+  const spawnObject = new THREE.Mesh(
+      spawnObjectGeometry, spawnObjectMaterial,
+  );
+  spawnObject.position.set(-15, 4, 0);
+  scene.add( spawnObject );
+
+  createZConveyorBelt(-15, 0, -10);
+  createZConveyorBelt(15, 0, -10);
+  createZConveyorBelt(15, 0, -10);
+  createXConveyorBelt(-7.5, 0, -15);
+  createXConveyorBelt(7.5, 0, -15);
+}
+
+function createWall(x, y, z, width, height, depth) {
+  const geometry = new THREE.BoxGeometry(width, height, depth);
+  const material = new THREE.MeshLambertMaterial({color: 0x61676a});
+
+  const wall = new THREE.Mesh(geometry, material);
+  wall.position.set(x, y, z);
+
+  scene.add(wall);
+}
+
 function main() {
-  /* const canvas = document.querySelector('#glcanvas');
-
-  const gl = canvas.getContext('webgl');
-
-  if (!gl) {
-    console.log('WebGL not supported, falling back on experimental-webgl');
-    gl = canvas.getContext('experimental-webgl');
-  }
-
-  if (!gl) {
-    alert('Unable to initialize WebGL.' +
-        'Your browser or machine may not support it.');
-    return;
-  }
-
-  // Set clear color to black, fully opaque
-  // gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  // Clear the color buffer with specified clear color
-  // gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-  //
-  // Create shaders
-  //
-  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-  gl.shaderSource(vertexShader, vertexShaderText);
-  gl.shaderSource(fragmentShader, fragmentShaderText);
-
-  gl.compileShader(vertexShader);
-  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-    console.error(
-        'ERROR compiling vertex shader!',
-        gl.getShaderInfoLog(vertexShader),
-    );
-    return;
-  }
-
-  gl.compileShader(fragmentShader);
-  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-    console.error(
-        'ERROR compiling fragment shader!',
-        gl.getShaderInfoLog(fragmentShader),
-    );
-    return;
-  }
-
-  const program = gl.createProgram();
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    console.error('ERROR linking program!', gl.getProgramInfoLog(program));
-    return;
-  }
-
-  gl.validateProgram(program);
-  if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
-    console.error('ERROR validating program!', gl.getProgramInfoLog(program));
-    return;
-  }*/
+  // Se crean el piso, el techo y las 4 paredes
+  createWall(0, 0, 0, 40, 1, 40);
+  createWall(0, 20, 0, 41, 1, 40);
+  createWall(-20, 10, 0, 1, 20, 40);
+  createWall(20, 10, 0, 1, 20, 40);
+  createWall(0, 10, -20, 41, 20, 1);
 
 
-  /* ========= EJEMPLO THREE JS =========== */
-  const scene = new THREE.Scene();
-  const camera = new THREE
-      .PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+  const light = new THREE.PointLight(0xFFFFFF, 1.8, 500);
+  light.position.set(0, 17, 0);
+  scene.add(light);
 
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setClearColor('#000000');
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
+  camera.position.y = 25;
+  camera.position.z = 30;
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-  const cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
+  const controls = new OrbitControls(camera, renderer.domElement);
 
-  camera.position.z = 5;
+  const gridXZ = new THREE.GridHelper(100, 10,
+      new THREE.Color(0xff0000), new THREE.Color(0xffffff));
+  scene.add(gridXZ);
+
+  objectSetup(scene);
 
   const animate = function() {
+    controls.update();
     requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // light.position.x = (light.position.x + 0.5) % 100;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
 
     renderer.render( scene, camera );
   };
-
   animate();
 }
+
 
 main();
