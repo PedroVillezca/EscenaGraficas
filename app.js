e7648292c2cff86bab5e34bfe17adabfe8e01014
Gibran Gonzalez <hector.gibran@hotmail.com>
2021-01-26 20:28:49 -0600

    Increases scene size, adds path for conveyor belt

--- app.js
+++ app.js
@@ -11,22 +11,6 @@ import {OrbitControls} from
 
 const scene = new THREE.Scene();
 
-const camera = new THREE
-    .PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setClearColor('#000000');
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-window.addEventListener('resize', () => {
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  camera.aspect = window.innerWidth / window.innerHeight;
-
-  camera.updateProjectionMatrix();
-});
-
-
 /**
  * Creates a conveyor belt on x,y,z coordinates and
  * rotates it in y in terms of angle given
@@ -36,7 +20,7 @@ window.addEventListener('resize', () => {
  * @param {Number} angle
  */
 function createZConveyorBelt(x, y, z) {
-  const mainStructureGeom = new THREE.BoxGeometry(5, 0.5, 15);
+  const mainStructureGeom = new THREE.BoxGeometry(5, 0.5, 30);
   const mainStructureMaterial = new THREE
       .MeshLambertMaterial({color: 0xbfc7cc});
 
@@ -50,10 +34,10 @@ function createZConveyorBelt(x, y, z) {
   const legTwo = new THREE.Mesh(legGeom, mainStructureMaterial);
   const legThree = new THREE.Mesh(legGeom, mainStructureMaterial);
   const legFour = new THREE.Mesh(legGeom, mainStructureMaterial);
-  legOne.position.set(-2+x, 1.5+y, -7+z);
-  legTwo.position.set(2+x, 1.5+y, -7+z);
-  legThree.position.set(2+x, 1.5+y, 7+z);
-  legFour.position.set(-2+x, 1.5+y, 7+z);
+  legOne.position.set(-2+x, 1.5+y, -12+z);
+  legTwo.position.set(2+x, 1.5+y, -12+z);
+  legThree.position.set(2+x, 1.5+y, 12+z);
+  legFour.position.set(-2+x, 1.5+y, 12+z);
   scene.add(legOne);
   scene.add(legTwo);
   scene.add(legThree);
@@ -61,7 +45,7 @@ function createZConveyorBelt(x, y, z) {
 }
 
 function createXConveyorBelt(x, y, z) {
-  const mainStructureGeom = new THREE.BoxGeometry(10, 0.5, 5);
+  const mainStructureGeom = new THREE.BoxGeometry(15, 0.5, 5);
   const mainStructureMaterial = new THREE
       .MeshLambertMaterial({color: 0xbfc7cc});
 
@@ -69,39 +53,6 @@ function createXConveyorBelt(x, y, z) {
       .Mesh(mainStructureGeom, mainStructureMaterial);
   mainStructure.position.set(0+x, 3+y, 0+z);
   scene.add(mainStructure);
-
-  const legGeom = new THREE.CylinderGeometry(0.5, 0.5, 3);
-  const legOne = new THREE.Mesh(legGeom, mainStructureMaterial);
-  const legTwo = new THREE.Mesh(legGeom, mainStructureMaterial);
-  const legThree = new THREE.Mesh(legGeom, mainStructureMaterial);
-  const legFour = new THREE.Mesh(legGeom, mainStructureMaterial);
-  legOne.position.set(-4+x, 1.5+y, -2+z);
-  legTwo.position.set(4+x, 1.5+y, -2+z);
-  legThree.position.set(4+x, 1.5+y, 2+z);
-  legFour.position.set(-4+x, 1.5+y, 2+z);
-  scene.add(legOne);
-  scene.add(legTwo);
-  scene.add(legThree);
-  scene.add(legFour);
-}
-
-function objectSetup() {
-  const spawnObjectGeometry = new THREE.BoxGeometry(5, 8, 5);
-  const spawnObjectMaterial = new THREE.MeshLambertMaterial(
-      {color: 0xbfc7cc},
-  );
-
-  const spawnObject = new THREE.Mesh(
-      spawnObjectGeometry, spawnObjectMaterial,
-  );
-  spawnObject.position.set(-15, 4, 0);
-  scene.add( spawnObject );
-
-  createZConveyorBelt(-15, 0, -10);
-  createZConveyorBelt(15, 0, -10);
-  createZConveyorBelt(15, 0, -10);
-  createXConveyorBelt(-7.5, 0, -15);
-  createXConveyorBelt(7.5, 0, -15);
 }
 
 function createWall(x, y, z, width, height, depth) {
@@ -114,31 +65,76 @@ function createWall(x, y, z, width, height, depth) {
   scene.add(wall);
 }
 
-function main() {
+function objectSetup() {
+  const spawnObjectGeometry = new THREE.BoxGeometry(5, 8, 5);
+  const spawnObjectMaterial = new THREE.MeshLambertMaterial(
+      {color: 0xbfc7cc},
+  );
+
+  const spawnObject = new THREE.Mesh(
+      spawnObjectGeometry, spawnObjectMaterial,
+  );
+  spawnObject.position.set(-30, 4, 15);
+  scene.add( spawnObject );
+
   // Se crean el piso, el techo y las 4 paredes
-  createWall(0, 0, 0, 40, 1, 40);
-  createWall(0, 20, 0, 41, 1, 40);
-  createWall(-20, 10, 0, 1, 20, 40);
-  createWall(20, 10, 0, 1, 20, 40);
-  createWall(0, 10, -20, 41, 20, 1);
+  createWall(0, 0, 0, 70, 1, 40);
+  createWall(0, 20, 0, 71, 1, 40);
+  createWall(-35, 10, 0, 1, 20, 40);
+  createWall(35, 10, 0, 1, 20, 40);
+  createWall(0, 10, -20, 71, 20, 1);
 
+  // Objetos para la conveyor belt
+  createZConveyorBelt(-30, 0, -2.5);
+  createZConveyorBelt(30, 0, -2.5);
 
-  const light = new THREE.PointLight(0xFFFFFF, 1.8, 500);
-  light.position.set(0, 17, 0);
-  scene.add(light);
+  createZConveyorBelt(-10, 0, -2.5);
+  createZConveyorBelt(10, 0, -2.5);
 
+  createXConveyorBelt(-20, 0, -15);
+  createXConveyorBelt(20, 0, -15);
+
+  createXConveyorBelt(0, 0, 10);
+}
+
+function main() {
+/*
+ Se crea la camara para ver la escena, se le da una posicion propia
+ y un punto inicial al cual mirar
+*/
+  const camera = new THREE
+      .PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
   camera.position.y = 25;
   camera.position.z = 30;
   camera.lookAt(new THREE.Vector3(0, 0, 0));
 
+  // Se crea el renderiazdor, que a su vez genera el canvas para la escena
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setClearColor('#000000');
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  // Evento por si acaso las dimensiones de la pantalla llegasen a cambiar
+  window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+  });
+
+  // Se agregan los controles para la camara
   const controls = new OrbitControls(camera, renderer.domElement);
 
+  // Se crean las lineas del sistema de coordenadas
   const gridXZ = new THREE.GridHelper(100, 10,
       new THREE.Color(0xff0000), new THREE.Color(0xffffff));
   scene.add(gridXZ);
 
+  // Funcion para crear todos los objetos en la escena
   objectSetup(scene);
 
+
+  // Funcion que maneja las animaciones de los objetos
   const animate = function() {
     controls.update();
     requestAnimationFrame( animate );
@@ -149,6 +145,12 @@ function main() {
 
     renderer.render( scene, camera );
   };
+
+  // Fuente de luz para testing
+  const light = new THREE.PointLight(0xFFFFFF, 1.8, 500);
+  light.position.set(0, 17, 0);
+  scene.add(light);
+
   animate();
 }
 
